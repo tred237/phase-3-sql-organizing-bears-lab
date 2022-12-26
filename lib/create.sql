e9aa6bc23bf29d15a6af250fc4ed0cf0a4ddb2c2
trevor dewindt <trevor.dewindt@gmail.com>
2022-12-26 14:00:46 -0700

    commit tree 87b82139caa8dee68b9664a4557a99dc85951456

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id integer primary key,
+    name text,
+    age integer,
+    sex text,
+    color text,
+    temperament text,
+    alive boolean
+); 
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,19 +1,19 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "select name, age from bears where sex = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "select name from bears order by name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "select name, age from bears where alive order by age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "select name, age from bears order by age desc limit 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "select name, age from bears order by age limit 1;"
 end
