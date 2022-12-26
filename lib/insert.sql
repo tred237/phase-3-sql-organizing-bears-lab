845b904b9ea84220151fa89b1da939f0b2ed810d
trevor dewindt <trevor.dewindt@gmail.com>
2022-12-26 14:01:10 -0700

    final code

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+insert into bears (name, age, sex, color, temperament, alive)
+values
+("Mr. Chocolate", 1, "M", "Br", "M", TRUE),
+("Rowdy", 2, "M", "B", "F", TRUE),
+("Tabitha", 3, "F", "B", "M", TRUE),
+("Sergeant Brown", 4, "M", "Br", "F", TRUE),
+("Melissa", 4, "F", "B", "M", TRUE),
+("Grinch", 4, "M", "Br", "F", TRUE),
+("Wendy", 3, "F", "Br", "M", FALSE),
+(NULL, 1, "F", "Br", "F", TRUE)
